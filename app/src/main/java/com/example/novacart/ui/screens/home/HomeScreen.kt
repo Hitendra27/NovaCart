2271a469ab9512f5f51aa025d63a7a609d429f72
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2026-09-14 19:24:56 +0400

    feat: implemented search Icon and cart Icon in HomeScreen.kt

--- app/src/main/java/com/example/novacart/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/novacart/ui/screens/home/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.example.novacart.ui.screens.home
 
+import android.R
+import android.R.style
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,7 +20,13 @@ import com.example.novacart.ui.viewmodel.ProductViewModel
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
 import com.example.novacart.ui.components.ProductCard
@@ -36,10 +44,39 @@ fun HomeScreen(
         topBar = {
             TopAppBar(
                 title = {
-                    Text("NovaCart")
+                Text(
+                    text = "NovaCart",
+                    style = MaterialTheme.typography.titleLarge
+            )
+        },
+                actions = {
+
+                    IconButton(
+                        OnClick = {
+                            // Search - coming next
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Search"
+                        )
+                    }
+
+                    IconButton(
+                        onClick = {
+                            // Cart - coming later
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ShoppingCart,
+                            contentDescription = "Shopping Cart"
+                        )
+                    }
                 }
             )
+
         }
+
     ) { innerPadding ->
 
         when {
